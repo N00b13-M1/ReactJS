9027e2e0e6fa4452bc2806fe562c6c6c15b515b2
N00b13-M1 <michael.cornelis@gmail.com>
2022-01-26 15:54:56 +0100

    DatesinJSPHP&Human

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
           <p>Temps Javascripts {tempsJavascript} millisecondes</p>
           <p>Temps PHP {tempsPHP/1000} millisecondes</p>
-          <p>Today is {dateCourante.getMonth()}/{dateCourante.getMonth()+1}/{year} millisecondes</p>
+          <p>Today is {day}/{month+1}/{year} millisecondes</p>
         </header>
       </div>
       <footer>Molengeek TM</footer>
