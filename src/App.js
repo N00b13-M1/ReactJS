df9232492d6975cd1615404f48b26f321a91080d
N00b13-M1 <michael.cornelis@gmail.com>
2022-01-26 15:57:18 +0100

    Removed variables and imports

--- src/App.js
+++ src/App.js
@@ -1,27 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  let dateCourante = new Date();
-  let tempsJavascript = dateCourante.getTime();
-  let tempsPHP = tempsJavascript
-  
-  let day = dateCourante.getDate();
-  let month = dateCourante.getMonth();
-  let year = dateCourante.getFullYear();
-
   return (
-    <div>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Temps Javascripts {tempsJavascript} millisecondes</p>
-          <p>Temps PHP {tempsPHP/1000} millisecondes</p>
-          <p>Today is {day}/{month+1}/{year} millisecondes</p>
-        </header>
-      </div>
-      <footer>Molengeek TM</footer>
-    </div>
+  'Hello world'
   );
 }
 
