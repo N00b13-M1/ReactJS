434c78b7158209b85b5ffc03433a27e17d78c4d6
N00b13-M1 <michael.cornelis@gmail.com>
2022-01-26 16:28:29 +0100

    CommitDates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 
 function App() {
   return (
-  'Hello world'
+    <div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Temps Javascripts {tempsJavascript} millisecondes</p>
+          <p>Temps PHP {tempsPHP/1000} millisecondes</p>
+          <p>Today is {dateCourante.getMonth()}/{dateCourante.getMonth()+1}/{year} millisecondes</p>
+        </header>
+      </div>
+      <footer>Molengeek TM</footer>
+    </div>
   );
 }
 
