2b6ec483b62f37484c881e12c9abb9073ff30b21
N00b13-M1 <michael.cornelis@gmail.com>
2022-01-26 16:22:14 +0100

    COmmit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 
 function App() {
+  let dateCourante = new Date();
+  let tempsJavascript = dateCourante.getTime();
+  let tempsPHP = tempsJavascript
+  
+  let day = dateCourante.getDate();
+  let month = dateCourante.getMonth();
+  let year = dateCourante.getFullYear();
+
   return (
 
     "Molengeek TM"
